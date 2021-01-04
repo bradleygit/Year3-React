b09a2edfd7e2c77a648987bdcf6134512dc2fe56
bradleygit <bradley.slater@northumbria.ac.uk>
2021-01-04 00:44:02 +0000

    Basic web app for days

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
+
+
+class Day extends React.Component{
+    state = {
+        displayData:false
+    }
+
+    handleDayClick = () => {
+        this.setState({displayData:!this.state.displayData})
+    }
+
+    render(){
+        let additionalData = "";
+        if (this.state.displayData) {
+            additionalData = <h1>Test!</h1>
+        }
+        return (
+            <div className="Day">
+                <h2 onClick={this.handleDayClick}>{this.props.day}</h2>
+                {additionalData}
+            </div>
+        );
+
+    }
+}
+
+
+class Schedule extends React.Component{
+    render(){
+        const days = ["Monday","Tuesday","Wednesday","Thursday"]
+
+        return(
+            <div className="daysContainer">
+            {days.map((day,i)=>(<Day key={i} day={day} />))}
+        </div>
+        );
+    }
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1 className="Title">Schedule</h1>
+            <Schedule/>
+        </div>
+    );
 }
 
 export default App;
