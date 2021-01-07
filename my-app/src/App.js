bec5eb69454257a011f01aa5d33d191e5b949c8d
bradleygit <bradley.slater@northumbria.ac.uk>
2021-01-07 15:12:28 +0000

    Some placement code for schedule

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,37 +1,103 @@
 import React from 'react';
 import './App.css';
 
+class Session extends React.Component{
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            data:this.props.data
+        }
+    }
+
+    render(){
+        console.log();
+        return (
+            <div className="Session">
+                <p>{this.state.data.type}</p>
+            </div>
+        );
+    }
+}
 
 class Day extends React.Component{
-    state = {
-        displayData:false
-    }
 
     handleDayClick = () => {
         this.setState({displayData:!this.state.displayData})
     }
 
-    render(){
-        let additionalData = "";
-        if (this.state.displayData) {
-            additionalData = <h1>Test!</h1>
+
+    componentDidMount(){
+        this.setState({day:this.props.day})
+        let url = "http://localhost/Assignment3/api/schedule?day="+this.state.day;
+        this.fetchFromURL(url);
+    }
+
+
+    fetchFromURL = (url) => {
+        fetch(url)
+            .then((response) => response.json())
+            .then((data) => {
+                this.setState({data:data.data})
+            })
+            .catch((err) => {
+                    console.log("something went wrong ", err)
+                    return [];
+                }
+            );
+    }
+
+
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            day:this.props.day,
+            displayData:false,
+            data:[]
         }
+    }
+    render(){
         return (
             <div className="Day">
                 <h2 onClick={this.handleDayClick}>{this.props.day}</h2>
-                {additionalData}
+                {this.state.displayData ? this.state.data.map((data,i)=>(<Session key={i} data={data}/>)) : ""}
             </div>
         );
 
     }
+
 }
 
+function test(){
+
+    let myJSON = {"username":"John", "password":"top_secret!"};
+    const postJSON = (api,json) => {
+        fetch(api, {
+            method: 'POST',
+            headers : new Headers(),
+            body:JSON.stringify(json)})
+            .then(
+                function (response) {
+                    return response.json();
+                })
+            .then(
+                function(data) {
+                    console.log(data);
+                })
+            .catch(function (err) {
+                    console.log("Something went wrong!", err);
+                }
+            );
+    }
+
+    
+}
 
 class Schedule extends React.Component{
+
     render(){
         const days = ["Monday","Tuesday","Wednesday","Thursday"]
-
         return(
             <div className="daysContainer">
             {days.map((day,i)=>(<Day key={i} day={day} />))}
@@ -39,6 +105,7 @@ class Schedule extends React.Component{
         );
     }
 }
+
 
 function App() {
     return (
